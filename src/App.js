0e47ad2ebb01d7336fc0c508851861fc00397186
vietpham-stvg <82923647+phamquangviet891@users.noreply.github.com>
2023-11-29 15:56:04 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [message, setMessage] = useState();
+  const [connection, setConnection] = useState(null);
+
+  useEffect(async () => {
+   const a= await connect();
+  }, []);
+
+  const connect = async () => {
+    try {
+      const newConnection = await new HubConnectionBuilder()
+        .withUrl('https://noti-service.onrender.com/NotiHub') // Thay thế bằng URL của ứng dụng ASP.NET Core của bạn
+        .withAutomaticReconnect()
+        .build();
+      newConnection.on('GetListNoti', (message) => {
+        setMessage(message);
+        //console.log("GetListNoti: ", message)
+      })
+      newConnection.onclose(e => {
+        setConnection();
+      });
+      //console.log("newconnection:", newConnection)
+      await newConnection.start();
+      await newConnection.invoke("UpdateUser", 'user');
+      await newConnection.invoke("ConnectUser");
+      setConnection(newConnection);
+      //console.log("build thanh cong, newconnection:", newConnection)
+      setConnection(newConnection);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  const disconnect = async () => {
+    try {
+      if (connection) { await connection.stop(); }
+      setMessage();
+    }
+    catch (e) { console.log(e) }
+
+  }
+  
+return (
+  <div>
+    <ul>
+      {message}
+    </ul>
+    <div>
+      <button onClick={() => connect()}>Connect</button>
+      <button onClick={() => disconnect()}>disconnect</button>
     </div>
-  );
-}
+  </div>
+);
+};
 
 export default App;
